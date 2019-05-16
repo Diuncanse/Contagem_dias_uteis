471ce5b86c7bb1606be37bb95b07dbf06aa355d7
Leonardo da Silva Diuncanse <leodiunka@gmail.com>
2019-05-16 11:00:20 -0300

    Atualização dias_uteis2.php
    Acrescentado a função de contar os feriados.

--- dias_uteis/dias_uteis2.php
+++ dias_uteis/dias_uteis2.php
@@ -15,27 +15,28 @@ $lastday  = ($_POST ["Tdesl"]);
 $nr_work_days = getWorkingDays($beginday, $lastday);
 echo $nr_work_days;
 
-function getWorkingDays($startDate, $endDate)
-{
+function getWorkingDays($startDate, $endDate) {
     $begin = strtotime($startDate);
     $end   = strtotime($endDate);
     if ($begin > $end) {
-        echo "startdate is in the future! <br />";
-
         return 0;
-    } else {
-        $no_days  = 0;
+    }
+    else {
+        $holidays = array('01/01', '04/03', '05/03', '19/04', '21/04', '01/05', '20/06', '07/09', '12/10', '02/11', '15/11', '25/12');
         $weekends = 0;
+        $no_days = 0;
+        $holidayCount = 0;
         while ($begin < $end) {
             $no_days++; // no of days in the given interval
             $what_day = date("N", $begin);
             if ($what_day > 5) { // 6 and 7 are weekend days
                 $weekends++;
+            } elseif (in_array(date("d/m", $begin), $holidays)) {
+                $holidayCount++;
             };
-            // +1 day
-            $begin += 86400; 
+            $begin += 86400; // +1 day
         };
-        $working_days = $no_days - $weekends;
+        $working_days = $no_days - $weekends - $holidayCount;
 
         return $working_days;
     }
